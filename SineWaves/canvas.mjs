5619f06b2189ecc8fa463100c82a4cc11a8a1f1c
Simon Cao <simon.cao2007@gmail.com>
2024-05-03 23:32:12 -0400

    Imported dat.gui module

--- SineWaves/canvas.mjs
+++ SineWaves/canvas.mjs
@@ -1,3 +1,9 @@
+// Import Dat.gui
+// const dat = require('dat.gui');
+import dat from 'dat.gui';
+
+const gui = new dat.GUI();
+
 // Selecting the first canvas element in HTML file
 const canvas = document.querySelector('canvas');
 
@@ -18,3 +24,12 @@ function clearScreen() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.closePath();
 }
+
+ctx.beginPath();
+ctx.moveTo(0, canvas.height / 2);
+for (let i = 0; i < canvas.width; i++) {
+    const amplitude = 100;
+    const period = 0.007;
+    ctx.lineTo(i, canvas.height / 2 + Math.sin(i * period) * amplitude);
+}
+ctx.stroke();
